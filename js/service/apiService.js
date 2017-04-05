8b38c21cb750fcfc63b10b3aa18ea0c561b6bd5f
Min Cheoul Kim <mincheoulkim@google.com>
2017-04-04 18:51:37 -0700

    First commit to enable Buganizer API

---
+++ js/service/apiService.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mincheoulkim on 4/4/17.
+ */
+
+
+(function(angular) {
+    'use strict';
+
+    angular.module('ngAndroidNext99')
+        .service('apiSerivce', function() {
+            var sAPIKey = 'addd';
+        });
+
+})(window.angular);
